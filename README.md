ac1d278d9625da1fd00483708f4d2a7ff71aac63
Adrien Etienne <a.etienne92@gmail.com>
2021-02-17 16:37:29 +0100

    docs: update readme

--- README.md
+++ README.md
@@ -2,6 +2,21 @@
 
 Stronghold Pay SDK for React Native
 
+- [react-native-strongholdpay-sdk](#react-native-strongholdpay-sdk)
+  - [Installation](#installation)
+  - [Usage](#usage)
+    - [Import the components](#import-the-components)
+    - [`AddPaymentSource`](#addpaymentsource)
+    - [`UpdatePaymentSource`](#updatepaymentsource)
+    - [`Charge`](#charge)
+    - [`Tip`](#tip)
+  - [How to run the example](#how-to-run-the-example)
+    - [Install the dependencies](#install-the-dependencies)
+    - [Configure the example](#configure-the-example)
+    - [Starts the demo](#starts-the-demo)
+  - [Contributing](#contributing)
+  - [License](#license)
+
 ## Installation
 
 ```sh
@@ -10,12 +25,103 @@ npm install react-native-strongholdpay-sdk
 
 ## Usage
 
-```js
-import StrongholdpaySdk from "react-native-strongholdpay-sdk";
+This package is based on the [Stronghold Pay Javascript SDK](https://docs.strongholdpay.com/stronghold-pay-js). Check the official documentation for more information.
 
-// ...
+### Import the components
 
-const result = await StrongholdpaySdk.multiply(3, 7);
+```typescript
+import {
+  AddPaymentSource,
+  UpdatePaymentSource,
+  Charge,
+  Tip,
+} from 'react-native-strongholdpay-sdk';
+```
+
+### `AddPaymentSource`
+
+```typescript
+<AddPaymentSource
+  publishableKey={publishableKey}
+  customerToken={customerToken}
+  onPress={() => console.log('on press AddPaymentSource')}
+  onExit={() => console.log('on exit')}
+  onEvent={(event) => console.log('on event', JSON.stringify(event))}
+  onSuccess={(paymentSource) => {
+    console.log('add payment source successful', JSON.stringify(paymentSource));
+  }}
+  onError={(error) => console.log('on error', JSON.stringify(error))}
+  onReady={() => console.log('on ready')}
+>
+  <Button
+    title="Add Payment Source"
+    onPress={() => console.log('onPress child button')}
+  />
+</AddPaymentSource>
+```
+
+### `UpdatePaymentSource`
+
+```typescript
+<UpdatePaymentSource
+  publishableKey={publishableKey}
+  customerToken={customerToken}
+  data={{
+    paymentSourceId,
+  }}
+  onSuccess={() => console.log('update payment source successful')}
+>
+  <Button
+    title="Update Payment Source"
+    onPress={() => console.log('onPress child button')}
+  />
+</UpdatePaymentSource>
+```
+
+### `Charge`
+
+```typescript
+<Charge
+  publishableKey={publishableKey}
+  customerToken={customerToken}
+  data={{
+    charge: { amount: 4950, paymentSourceId, externalId: '12345679' },
+    tip: {
+        amount: 200,
+        beneficiaryName: 'My Budtender',
+    },
+  }}
+  onSuccess={(charge) => {
+    console.log('charge creation successful', JSON.stringify(charge));
+  }}
+>
+  <Button title="Charge" onPress={() => console.log('onPress child button')} />
+</Charge>
+```
+
+### `Tip`
+
+```typescript
+<Tip
+  publishableKey={publishableKey}
+  customerToken={customerToken}
+  data={{
+    tip: {
+      amount: 300,
+      paymentSourceId: paymentSourceId,
+      chargeId: chargeId,
+      beneficiaryName: 'My Budtender',
+    },
+  }}
+  onSuccess={(tip) => {
+    console.log('tip creation successful', JSON.stringify(tip));
+  }}
+>
+  <Button
+    title="Tip"
+    onPress={() => console.log('onPress child button')}
+  />
+</Tip>
 ```
 
 ## How to run the example
@@ -33,7 +139,8 @@ yarn install
 const publishableKey = 'pk_sandbox_0ERwWH6SHT8L2iKMniifHed8';
 
 // Set the customer token generated using the API
-const customerToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiJjdXN0b21lcl9IcHdBbG9FbnAxUTYwb0Y4cTNnT0pMeFYiLCJuYmYiOjE2MTM1Njk4NDIsImV4cCI6MTYxMzYxMzA0MiwiaWF0IjoxNjEzNTY5ODQyfQ.GKTFqF5egEc2Z2Gasf-xvXeXU_gFhTf7MCdK6Zql5ZE';
+const customerToken =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiJjdXN0b21lcl9IcHdBbG9FbnAxUTYwb0Y4cTNnT0pMeFYiLCJuYmYiOjE2MTM1Njk4NDIsImV4cCI6MTYxMzYxMzA0MiwiaWF0IjoxNjEzNTY5ODQyfQ.GKTFqF5egEc2Z2Gasf-xvXeXU_gFhTf7MCdK6Zql5ZE';
 ```
 
 ### Starts the demo
--- example/src/App.tsx
+++ example/src/App.tsx
@@ -16,19 +16,21 @@ import {
   Tip,
 } from 'react-native-strongholdpay-sdk';
 
-const host = 'http://192.168.1.202:5002';
+const host: string | undefined = undefined;
 
 // Set your publishable key here
 const publishableKey = 'pk_sandbox_0ERwWH6SHT8L2iKMniifHed8';
 
 // Set the customer token generated using the API
 const customerToken =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiJjdXN0b21lcl9qenBoa2JHRVNyQjNNbU5hQTlBUXRBM00iLCJuYmYiOjE2MTM0OTIzODksImV4cCI6MTYxMzUzNTU4OSwiaWF0IjoxNjEzNDkyMzg5fQ.uUcDjPUN1vLBBhraeIA7374hzmTV6iOXMydd9svgS54';
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiJjdXN0b21lcl9IcHdBbG9FbnAxUTYwb0Y4cTNnT0pMeFYiLCJuYmYiOjE2MTM1NzM2ODQsImV4cCI6MTYxMzYxNjg4NCwiaWF0IjoxNjEzNTczNjg0fQ.g7qu8dbFPMpVJojcghvbAsxiTn6wGhdwCgjw23OBQRk';
 
 const Separator = () => <View style={styles.separator} />;
 
 export default function App() {
-  const [paymentSourceId, setPaymentSourceId] = useState(null);
+  // Set to null by default, a sandbox payment source ID can be given here
+  const [paymentSourceId, setPaymentSourceId] = useState('');
+  const [chargeId, setChargeId] = useState('');
 
   return (
     <SafeAreaView style={styles.container}>
@@ -49,13 +51,14 @@ export default function App() {
           onExit={() => console.log('on exit')}
           onEvent={(event) => console.log('on event', JSON.stringify(event))}
           onSuccess={(paymentSource) => {
-            console.log('add payment source successful');
-            console.log(JSON.stringify(paymentSource));
+            console.log(
+              'add payment source successful',
+              JSON.stringify(paymentSource)
+            );
             setPaymentSourceId(paymentSource.id);
           }}
           onError={(error) => {
-            console.log('on error');
-            console.log(JSON.stringify(error));
+            console.log('on error', JSON.stringify(error));
           }}
           onReady={() => console.log('on ready')}
         >
@@ -64,9 +67,7 @@ export default function App() {
             onPress={() => {
               console.log('onPress child button');
             }}
-          >
-            Add Payment Source
-          </Button>
+          />
         </AddPaymentSource>
       </View>
       <Separator />
@@ -91,9 +92,7 @@ export default function App() {
             title="Update Payment Source"
             onPress={() => console.log('onPress child button')}
             disabled={!paymentSourceId}
-          >
-            Update Payment Source
-          </Button>
+          />
         </UpdatePaymentSource>
       </View>
       <Separator />
@@ -101,25 +100,52 @@ export default function App() {
         The customer authorize Stronghold Pay to initiate a charge on his
         payment source.
       </Text>
-      <Charge
-        host={host}
-        publishableKey={publishableKey}
-        customerToken={customerToken}
-        data={{
-          charge: { amount: 4950, paymentSourceId },
-        }}
-        onSuccess={(charge) => {
-          console.log('charge creation successful');
-          console.log(JSON.stringify(charge));
-        }}
-      >
-        <Button
-          title="Charge"
-          onPress={() => console.log('onPress child button')}
+      <View style={styles.fixToText}>
+        <Charge
+          host={host}
+          publishableKey={publishableKey}
+          customerToken={customerToken}
+          data={{
+            charge: { amount: 4950, paymentSourceId },
+          }}
+          onSuccess={(charge) => {
+            console.log('charge creation successful', JSON.stringify(charge));
+            setChargeId(charge.id);
+          }}
+          disabled={!paymentSourceId}
         >
-          Charge
-        </Button>
-      </Charge>
+          <Button
+            title="Charge"
+            onPress={() => console.log('onPress child button')}
+            disabled={!paymentSourceId}
+          />
+        </Charge>
+        <Charge
+          host={host}
+          publishableKey={publishableKey}
+          customerToken={customerToken}
+          data={{
+            charge: { amount: 3000, paymentSourceId },
+            tip: {
+              amount: 200,
+              beneficiaryName: 'My Budtender',
+            },
+          }}
+          onSuccess={(charge) => {
+            console.log(
+              'charge with tipe creation successful',
+              JSON.stringify(charge)
+            );
+          }}
+          disabled={!paymentSourceId}
+        >
+          <Button
+            title="Charge with Tip"
+            onPress={() => console.log('onPress child button')}
+            disabled={!paymentSourceId}
+          />
+        </Charge>
+      </View>
       <Separator />
       <Text style={styles.title}>
         The customer authorize Stronghold Pay to add a tip on top of an initial
@@ -130,16 +156,23 @@ export default function App() {
         publishableKey={publishableKey}
         customerToken={customerToken}
         data={{
-          tip: { amount: 300, paymentSourceId, chargeId: '' },
+          tip: {
+            amount: 300,
+            paymentSourceId: paymentSourceId,
+            chargeId: chargeId,
+            beneficiaryName: 'My Budtender',
+          },
         }}
         onSuccess={(tip) => {
-          console.log('tip creation successful');
-          console.log(JSON.stringify(tip));
+          console.log('tip creation successful', JSON.stringify(tip));
         }}
+        disabled={!chargeId}
       >
-        <Button title="Tip" onPress={() => console.log('onPress child button')}>
-          Tip
-        </Button>
+        <Button
+          title="Tip"
+          onPress={() => console.log('onPress child button')}
+          disabled={!chargeId}
+        />
       </Tip>
     </SafeAreaView>
   );
@@ -155,6 +188,10 @@ const styles = StyleSheet.create({
     width: 60,
     height: 60,
     marginVertical: 20,
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   title: {
     textAlign: 'center',
